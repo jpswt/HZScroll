3baa1f4be07d95f7d1e17f6cce8bb55aeaa97c31
jpswt <joel_swt@yahoo.com>
2022-10-26 18:01:18 -0500

    create data

---
+++ src/Data.js
@@ -0,0 +1,42 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://images.unsplash.com/photo-1472213984618-c79aaec7fef0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+	},
+	{
+		id: 2,
+		img: 'https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+	},
+	{
+		id: 3,
+		img: 'https://images.unsplash.com/photo-1433048980017-63f162f662b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJlfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+	},
+	{
+		id: 4,
+		img: 'https://images.unsplash.com/photo-1591543620767-582b2e76369e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60',
+	},
+	{
+		id: 5,
+		img: 'https://images.unsplash.com/photo-1444021465936-c6ca81d39b84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60',
+	},
+	{
+		id: 6,
+		img: 'https://images.unsplash.com/photo-1511515828069-1e4853d8b336?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60',
+	},
+	{
+		id: 7,
+		img: 'https://images.unsplash.com/photo-1570030990547-f6b13f3062ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60',
+	},
+	{
+		id: 8,
+		img: 'https://images.unsplash.com/photo-1494791368093-85217fbbf8de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60',
+	},
+	{
+		id: 9,
+		img: 'https://images.unsplash.com/photo-1559244673-9cee88d551f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60',
+	},
+	{
+		id: 10,
+		img: 'https://images.unsplash.com/photo-1553856622-d1b352e9a211?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fG5hdHVyZXxlbnwwfDJ8MHx8&auto=format&fit=crop&w=800&q=60',
+	},
+];
